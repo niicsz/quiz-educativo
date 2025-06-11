fea768fd839646bf2a8bbddc54de20be50d4be26
niicsz <nicolasbbini@gmail.com>
2025-06-10 23:52:02 -0300

    chore: initial commit

---
+++ src/main/java/com/equipe8/quiz/QuizOdsApplication.java
@@ -0,0 +1,11 @@
+package com.equipe8.quiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizOdsApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(com.equipe8.quiz.QuizOdsApplication.class, args);
+	}
+}
---
+++ src/test/java/com/equipe8/quiz/QuizOdsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.equipe8.quiz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizOdsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
