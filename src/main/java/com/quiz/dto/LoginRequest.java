0e68d989a9e453930eee35cbf03266ba3f7f2ae8
niicsz <nicolasbbini@gmail.com>
2025-06-13 01:49:38 -0300

    feat: simplify LoginRequest and ResponderRequest by using Lombok for getters and setters

--- src/main/java/com/quiz/dto/LoginRequest.java
+++ src/main/java/com/quiz/dto/LoginRequest.java
@@ -1,31 +1,10 @@
 package com.quiz.dto;
 
+import lombok.Data;
+
+@Data
 public class LoginRequest {
   private String nome;
   private String email;
   private String senha;
-
-  public String getNome() {
-    return nome;
-  }
-
-  public void setNome(String nome) {
-    this.nome = nome;
-  }
-
-  public String getEmail() {
-    return email;
-  }
-
-  public void setEmail(String email) {
-    this.email = email;
-  }
-
-  public String getSenha() {
-    return senha;
-  }
-
-  public void setSenha(String senha) {
-    this.senha = senha;
-  }
 }
