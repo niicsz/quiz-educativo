31bb502ef6206d47528f95ae06f84a65de47e74d
niicsz <nicolasbbini@gmail.com>
2025-06-13 01:49:38 -0300

    feat: simplify LoginRequest and ResponderRequest by using Lombok for getters and setters

--- src/main/java/com/quiz/dto/ResponderRequest.java
+++ src/main/java/com/quiz/dto/ResponderRequest.java
@@ -1,31 +1,10 @@
 package com.quiz.dto;
 
+import lombok.Data;
+
+@Data
 public class ResponderRequest {
   private Long usuarioId;
   private Long perguntaId;
   private String resposta;
-
-  public Long getUsuarioId() {
-    return usuarioId;
-  }
-
-  public void setUsuarioId(Long usuarioId) {
-    this.usuarioId = usuarioId;
-  }
-
-  public Long getPerguntaId() {
-    return perguntaId;
-  }
-
-  public void setPerguntaId(Long perguntaId) {
-    this.perguntaId = perguntaId;
-  }
-
-  public String getResposta() {
-    return resposta;
-  }
-
-  public void setResposta(String resposta) {
-    this.resposta = resposta;
-  }
 }
